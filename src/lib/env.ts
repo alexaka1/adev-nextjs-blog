91e4a87b6b1387dbf2fe9c9281224a00337c7d97
Alex Martossy <github@mail.martossy.hu>
2025-05-10 14:48:04 +0200

    Add sentry (#13)
    * Add sentry
    
    * Add .env example

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,10 +1,14 @@
 import { createEnv } from '@t3-oss/env-nextjs';
 import { vercel } from '@t3-oss/env-core/presets-zod';
-// import { z } from 'zod';
+import { z } from 'zod';
 
 export const env = createEnv({
   server: {},
-  client: {},
-  experimental__runtimeEnv: {},
+  client: {
+    NEXT_PUBLIC_SENTRY_DSN: z.string(),
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  },
   extends: [vercel()],
 });
