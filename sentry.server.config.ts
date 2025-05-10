a193d34daddaf2abd8ad2ecbe3a1c93121c8b390
Alex Martossy <github@mail.martossy.hu>
2025-05-10 14:39:23 +0200

    Add sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,26 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import { init } from '@sentry/nextjs';
+import { type Integration } from '@sentry/core';
+import { env } from '@/lib/env';
+
+const integrations: Integration[] = [];
+// if (process.env.NODE_ENV !== 'development') {
+//   integrations.push(captureConsoleIntegration());
+// }
+
+init({
+  dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  integrations: integrations,
+});
