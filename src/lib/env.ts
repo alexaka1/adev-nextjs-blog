e79b0626d8eb0e2e43c87da48e38b0173e713578
Alex Martossy <github@mail.martossy.hu>
2025-05-31 10:29:35 +0200

    Use zod v4 and reduce trace sample rate

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,6 @@
 import { createEnv } from '@t3-oss/env-nextjs';
 import { vercel } from '@t3-oss/env-core/presets-zod';
-import { z } from 'zod';
+import { z } from 'zod/v4';
 
 export const env = createEnv({
   server: {},
