161b9476c2617eae0a118319ef2d79b861f1f508
Alex Martossy <github@mail.martossy.hu>
2025-06-01 14:20:48 +0200

    Add card

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,14 @@ import Link from 'next/link';
 import { getPaginatedBlogPosts } from '@/lib/blog';
 import { type Metadata } from 'next';
 import { Pagination } from '@/components/pagination';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 interface BlogIndexPageProps {
   searchParams: {
@@ -27,31 +35,36 @@ export default async function BlogIndexPage({
   } = await getPaginatedBlogPosts(currentPage, pageSize);
 
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="mb-8 text-4xl font-bold">Blog Posts</h1>
+    <article className="container mx-auto py-8">
+      <h1 className="mb-8 text-4xl font-bold capitalize">Blog posts</h1>
 
       <div className="space-y-8">
         {posts.length === 0 ?
           <p>No blog posts found.</p>
         : posts.map((post) => (
-            <article
-              key={post.slug}
-              className="rounded-lg border p-6 shadow-sm transition-all hover:shadow-md"
-            >
+            <article key={post.slug}>
               <Link href={`/blog/${post.slug}`} className="block">
-                <h2 className="mb-2 text-2xl font-bold hover:text-main">
-                  {post.title}
-                </h2>
-                <div className="mb-4 flex items-center space-x-2 text-sm text-gray-600 dark:text-gray-400">
-                  <span>By {post.author}</span>
-                  <span>•</span>
-                  <time dateTime={post.date}>
-                    {new Date(post.date).toLocaleDateString()}
-                  </time>
-                </div>
-                <div className="flex items-center justify-between">
-                  <span className="text-main hover:underline">Read more →</span>
-                </div>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>
+                      <h2>{post.title}</h2>
+                    </CardTitle>
+                    <CardDescription className={`flex items-center space-x-2`}>
+                      <span>By {post.author}</span>
+                      <span className={`sr-only`}>on</span>
+                      <span aria-hidden={true}>•</span>
+                      <time dateTime={post.date}>
+                        {new Date(post.date).toLocaleDateString()}
+                      </time>
+                    </CardDescription>
+                  </CardHeader>
+                  {/*<CardContent className="mb-4 flex items-center space-x-2 text-sm text-gray-600 dark:text-gray-400"></CardContent>*/}
+                  <CardFooter className="flex items-center justify-between">
+                    <span className="text-main hover:underline">
+                      Read more →
+                    </span>
+                  </CardFooter>
+                </Card>
               </Link>
             </article>
           ))
@@ -67,6 +80,6 @@ export default async function BlogIndexPage({
           />
         </div>
       )}
-    </div>
+    </article>
   );
 }
