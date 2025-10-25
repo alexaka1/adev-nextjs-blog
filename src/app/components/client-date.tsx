23e257efe5c966b5394d1a79dc08e1e71dfcbf82
Alex Martossy <github@mail.martossy.hu>
2025-10-25 13:56:02 +0200

    Remove client-date.tsx

--- src/app/components/client-date.tsx
+++ src/app/components/client-date.tsx
@@ -1,7 +1,3 @@
-'use client';
 export default function ClientDate({ date }: { date: string }) {
-  if (typeof window !== 'undefined') {
-    return <>{new Date(date).toLocaleDateString()}</>;
-  }
   return <>{date}</>;
 }
